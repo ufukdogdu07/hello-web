66368fc5cd992996c91f020a6a6b63b45b03701b
ufukdogdu07 <ufuk.dogdu@accenture.com>
2016-06-08 14:54:31 +0300

    test

--- src/com/accenture/HelloController.java
+++ src/com/accenture/HelloController.java
@@ -12,7 +12,6 @@ public class HelloController{
    @RequestMapping(method = RequestMethod.GET)
    public String printHello(ModelMap model) {
       model.addAttribute("message", "Hello Spring MVC Framework!");
-
       return "hello";
    }
 
