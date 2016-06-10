d07d31078b54cf728eff0b7a006baa3c042d0fbb
ufukdogdu07 <ufuk.dogdu@accenture.com>
2016-06-10 16:49:23 +0300

    test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,10 +9,55 @@ import com.accenture.CalcNumber;
 public class CalculatorTest {
     @Test
     public void evaluatesExpression() {
-      CalcNumber calculator = new CalcNumber();
-      calculator.setNumber1(5);
-      calculator.setNumber2(3);
-      int sum = calculator.getTotal();
-      assertEquals(7, sum);
+        CalcNumber calculator = new CalcNumber();
+        calculator.setNumber1(5);
+        calculator.setNumber2(3);
+        int sum = calculator.getTotal();
+        assertEquals(8, sum);
     }
-  }
+
+    @Test
+    public void evaluatesExpression2() {
+        CalcNumber calculator = new CalcNumber();
+        calculator.setNumber1(5);
+        calculator.setNumber2(3);
+        int sum = calculator.getTotal();
+        assertEquals(7, sum);
+    }
+
+    @Test
+    public void evaluatesExpression3() {
+        CalcNumber calculator = new CalcNumber();
+        calculator.setNumber1(5);
+        calculator.setNumber2(3);
+        int sum = calculator.getTotal();
+        assertEquals(7, sum);
+    }
+
+    @Test
+    public void evaluatesExpression4() {
+        CalcNumber calculator = new CalcNumber();
+        calculator.setNumber1(5);
+        calculator.setNumber2(3);
+        int sum = calculator.getTotal();
+        assertEquals(7, sum);
+    }
+
+    @Test
+    public void evaluatesExpression5() {
+        CalcNumber calculator = new CalcNumber();
+        calculator.setNumber1(5);
+        calculator.setNumber2(3);
+        int sum = calculator.getTotal();
+        assertEquals(7, sum);
+    }
+
+    @Test
+    public void evaluatesExpression6() {
+        CalcNumber calculator = new CalcNumber();
+        calculator.setNumber1(5);
+        calculator.setNumber2(3);
+        int sum = calculator.getTotal();
+        assertEquals(7, sum);
+    }
+}
