f2abf5d429d2d4f9aca613460dcd740aa4327c39
ufukdogdu07 <ufuk.dogdu@accenture.com>
2016-06-20 13:30:57 +0300

    test

--- WebContent/WEB-INF/jsp/hello.jsp
+++ WebContent/WEB-INF/jsp/hello.jsp
@@ -1,7 +1,7 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
 <html>
 <head>
-<title>Naber Hata var</title>
+<title>Merhaba Arkadaşlar</title>
 </head>
 <body>
 	<h2>${message}</h2>
--- src/com/accenture/HelloController.java
+++ src/com/accenture/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.ui.ModelMap;
 public class HelloController {
     @RequestMapping(method = RequestMethod.GET)
     public String printHello(ModelMap model) {
-        model.addAttribute("message", "Merhaba Oguzcan");
+        model.addAttribute("message", "Merhaba millet");
         return "hello";
     }
 }
